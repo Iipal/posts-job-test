8c8d49d1464e6982eb76da2b6f8c6cf972be53dc
Iipal <malkoleyplay@gmail.com>
2020-10-13 06:59:18 +0300

    New loader with styled components

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,9 +1,47 @@
+import styled, { keyframes } from "styled-components";
+
+const Frames = keyframes`
+  0% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 0px;
+    left: 0px;
+    width: 72px;
+    height: 72px;
+    opacity: 0;
+  }
+`;
+
+const LoaderStyles = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  margin-top: 50px;
+
+  > div {
+    position: absolute;
+    border: 4px solid #eee;
+    opacity: 1;
+    border-radius: 50%;
+    animation: ${Frames} 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  }
+
+  > div:nth-child(2) {
+    animation-delay: -0.5s;
+  }
+`;
+
 export default function Loader() {
   return (
-    <div className='d-flex justify-content-center'>
-      <div className='spinner-border m-5' role='status'>
-        <span className='sr-only'>Loading...</span>
-      </div>
-    </div>
+    <LoaderStyles className='lds-ripple'>
+      <div></div>
+      <div></div>
+    </LoaderStyles>
   );
 }
